d0a314443367183ff67e297eeb9136821660382e
rohitagr0310 <rohitagr2610@gmail.com>
2024-04-25 14:40:57 +0530

    24 april 2024 - java lab

---
+++ java_lab/Example1.java
@@ -0,0 +1,13 @@
+public class Example1 {
+    String name = "Rajesh";
+    int age = 20;
+
+    public static void main(String[] argv)
+    {
+    Example1 ex = new Example1();
+
+    System.out.println("Name: " + ex.name);
+    System.out.println("Age: " + ex.age);
+    }
+}
+
